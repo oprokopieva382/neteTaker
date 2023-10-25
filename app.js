5f7874d6171e6c97ce2f3679afccf870106dd731
Oksana(KsyFate) <oprokopieva382@gmail.com>
2023-10-24 19:11:36 -0500

    add Read notes from db.json and Add a new note to db.json

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const fs = require("fs");
 const path = require("path");
 const notesData = require("./db/db.json");
+const { v4: uuidv4 } = require("uuid");
 
 //instance of Express.js
 const app = express();
@@ -9,7 +11,10 @@ const PORT = 3001;
 // Static middleware pointing to the public folder
 app.use(express.static("public"));
 
-//html routes
+// Middleware for parsing JSON in request body
+app.use(express.json());
+
+//HTML routes
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "public/notes.html"))
 );
@@ -18,10 +23,18 @@ app.get("*", (req, res) =>
   res.sendFile(path.join(__dirname, "public/index.html"))
 );
 
-// read notes from db.json
+// Read notes from db.json
 app.get("/api/notes", (req, res) => res.json(notesData));
 
 // Add a new note to db.json
+app.post("api/notes", (res, req) => {
+  const newNote = res.body;
+  newNote.id = uuidv4();
+
+  notesData.unshift(newNote);
+
+  res.json(newNote);
+});
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
