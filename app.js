a69744c7d79dde1d98fa0670ccd88165022060ed
Oksana(KsyFate) <oprokopieva382@gmail.com>
2023-10-26 18:52:03 -0500

    remove no needed code

--- app.js
+++ app.js
@@ -21,44 +21,6 @@ app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "public/notes.html"))
 );
 
-// Read notes from db.json
-// app.get("/api/notes", (req, res) => {
-//   console.info(`GET /api/notes`);
-//   res.status(200).json(notesData);
-// });
-
-// Add a new note to db.json
-// app.post("/api/notes", (req, res) => {
-//   const newNote = req.body;
-//   newNote.id = uuidv4();
-//   notesData.unshift(newNote);
-
-//   fs.writeFileSync(
-//     path.join(__dirname, "db/db.json"),
-//     JSON.stringify(notesData, null, 2)
-//   );
-
-//   res.json(newNote);
-// });
-
-// Delete note by id from db.json
-// app.delete("/api/notes/:id", (req, res) => {
-//   let id = req.params.id;
-//   let updatedNotes = notesData.filter((note) => note.id !== id);
-
-//   if (updatedNotes.length < notesData.length) {
-//     notesData = updatedNotes;
-
-//     fs.writeFileSync(
-//       path.join(__dirname, "db/db.json"),
-//       JSON.stringify(notesData, null, 2)
-//     );
-//     res.status(200).send();
-//   } else {
-//     res.status(404).json({ message: "Note not found" });
-//   }
-// });
-
 //HTML route
 app.get("*", (req, res) =>
   res.sendFile(path.join(__dirname, "public/index.html"))
